2be8c9515211746a10afca646b0365fc21ed984b
Sandeep-508 <elsenothing321@gmail.com>
2024-02-21 12:05:20 +0530

    completed till the slick slider section

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+let btn = document.querySelector(".lg-view");
+let nav = document.querySelector("#menu");
+
+menu.addEventListener("click", () => {
+    btn.classList.toggle("show");
+});
+
+
+function changeFilter(clickedFilter) {
+    const filters = document.querySelectorAll('.filter');
+    filters.forEach(filter => filter.classList.remove('Color_change'));
+    clickedFilter.classList.add('Color_change');
+}
